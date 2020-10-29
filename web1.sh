115c26ce71b12788ec2e89dade6e2ef123ac4701
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-29 10:23:11 +0900

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+sudo su -
+
 apt-get update -y
 apt-get install -y apache2
+
 echo "<html>" > /var/www/html/index.html
 echo "<h1>Server1</h1>" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
