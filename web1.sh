273141ece2530b4223b06276a1492e54f09e48d6
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-29 10:18:41 +0900

    Create web1.sh
    sh server1

---
+++ web1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y apache2
+echo "<html>" > /var/www/html/index.html
+echo "<h1>Server1</h1>" >> /var/www/html/index.html
+echo "</html>" >> /var/www/html/index.html
+
+service apache2 restart
