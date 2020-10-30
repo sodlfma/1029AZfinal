d61b722d3b61f4f164a9626e5a5bf7b4620b4353
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-30 12:59:16 +0900

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -4,9 +4,8 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 
 sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Welcome</h1>" >> /var/www/html/index.html
 sudo echo "<h1>Server1</h1>" >> /var/www/html/index.html
-sudo echo "<h1>Welcome</h1>" >> /var/www/html/index.html
+echo "<img src=\"https://mblogthumb-phinf.pstatic.net/MjAxOTEyMDFfMjcw/MDAxNTc1MTY5MzI2NjQy.Alvv7RamNA68ARpwptBc9k9iH6ie0T_PqyNgwoy5CIwg.Y0QsJ4KgnA-35GTVIIkKYoXU8fCBaArrATaSTiOHfcIg.JPEG.the_pure/1575169325070.jpg?type=w800\">" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
 
 sudo service apache2 restart
