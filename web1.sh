88620aeebdab49d45c9d5f8c6a67c9d009027596
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-30 12:48:00 +0900

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 
 sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Server1</h1>" >> /var/www/html/index.html
+sudo echo "<h1>♪(*´θ｀)ノServer1♪(*´θ｀)ノ</h1>" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
 
 sudo service apache2 restart
